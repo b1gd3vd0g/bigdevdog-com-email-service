cecddaadcfc74d5a4ae847667454ef3986d674de
b1gd3vd0g <devin@bigdevdog.com>
2024-08-09 14:07:45 -0700

    Deal with sending a confirmation email to the site visitor.

--- sender.js
+++ sender.js
@@ -1,4 +1,4 @@
-const { buildContactFormEmail } = require('./builder');
+const { buildContactFormEmail, buildConfirmationEmail } = require('./builder');
 const { validateAll } = require('./validators');
 
 const rv = (status, info) => {
@@ -24,10 +24,38 @@ const sendContactFormEmail = async (name, email, phone, header, message) => {
   );
   try {
     const delivery = await transporter.sendMail(builtEmail);
-    return rv(200, { delivery: delivery });
+    if (delivery.accepted.length) {
+      // The message was built successfully!!
+      const builtConfirmation = buildConfirmationEmail(
+        v.name,
+        v.email,
+        v.phone,
+        v.header,
+        v.message
+      );
+      const transporter2 = require('./transporter');
+      try {
+        await transporter2.sendMail({
+          ...builtConfirmation,
+          to: v.email,
+          subject: 'Your email has been sent successfully from BigDevDog.com.'
+        });
+        return rv(200, {
+          message: 'Contact email and confirmation have been sent successfully.'
+        });
+      } catch (error) {
+        return rv(207, {
+          message:
+            `Contact email was sent successfully, but a confirmation ` +
+            `email could not be sent to the provided address ${v.email}. If this ` +
+            `address is incorrect, please resubmit your message. If this address ` +
+            `is correct, you can expect a response within 2 days.`
+        });
+      }
+    }
+    return rv(500, { message: 'Message was not sent for an unknown reason.' });
   } catch (error) {
     return rv(500, { error: error });
   }
 };
-
 module.exports = { sendContactFormEmail };
