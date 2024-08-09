3755cb30c716e7cb851754e459de381503b1e50f
b1gd3vd0g <devin@bigdevdog.com>
2024-08-09 12:59:54 -0700

    Added default fields to the transporter

--- transporter.js
+++ transporter.js
@@ -1,12 +1,19 @@
 const { createTransport } = require('nodemailer');
-const transporter = createTransport({
-  host: ENV.SMTP_HOST,
-  port: ENV.SMTP_PORT,
-  secure: true,
-  auth: {
-    user: ENV.AUTOMAILER_BDD_COM_USERNAME,
-    pass: ENV.AUTOMAILER_BDD_COM_PASSWORD
+const transporter = createTransport(
+  {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true,
+    auth: {
+      user: process.env.AUTOMAILER_BDD_COM_USERNAME,
+      pass: process.env.AUTOMAILER_BDD_COM_PASSWORD
+    }
+  },
+  {
+    from: `BigDevDog.com automailer <${process.env.AUTOMAILER_BDD_COM_USERNAME}>`,
+    to: process.env.DEVIN_BDD_COM_EMAIL,
+    subject: 'A customer has reached out via the Contact Form on BigDevDog.com!'
   }
-});
+);
 
 module.exports = transporter;
